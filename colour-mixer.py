fdd04cd35f89f9ade22bdc18720c4a0b30bee577
iliadril <xiliadril@gmail.com>
2022-05-23 23:50:49 +0200

    shebang

--- colour-mixer.py
+++ colour-mixer.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python3
+
 from ColourMixerLib.main import get_result
+import sys
+
+
+if sys.version_info[0] < 3:
+    raise Exception("Must be using Python 3")
+elif sys.version_info[1] < 10:
+    raise Exception("Must be using at least Python 3.10")
 
 if __name__ == '__main__':
     final_color = get_result()
